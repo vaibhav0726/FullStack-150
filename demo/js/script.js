ec91b2a87478d6a082ce789cb8f30d7e7a619b2f
vaibhav0726 <vaibhav.varshney2_cs20@gla.ac.in>
2022-09-21 10:22:44 +0530

    learning js

---
+++ demo/js/script.js
@@ -0,0 +1,13 @@
+function third(){
+    console.log("third function")
+}
+function second(){
+    console.log("second function")
+}
+function first(){
+    console.log("first function")
+}
+
+first()
+second()
+third()
